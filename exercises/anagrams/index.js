12210500c920449f6ac72c9f6119fa8b4dbbbcb5
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-13 14:26:21 -0800

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,19 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let newA = stringA.toLowerCase().replace(/[^\w]/g,"").split("");
+  let newB = stringB.toLowerCase().replace(/[^\w]/g,"").split("");
+  if(stringA.length!==stringB.length){
+    return false;
+  } else {
+    for(let char of newA){
+      if(!newB.includes(char)){
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
 module.exports = anagrams;
