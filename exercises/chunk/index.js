728f7b6c99a71eec21242953cc87eb93d8ec88a0
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-13 14:09:46 -0800

    completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/* function chunk(array, size) {
+  let newArr = [];
+  for(let i=0; i <array.length; i+=size){
+    newArr.push(array.slice(i,i+size));
+  }
+  return newArr;
+}*/
+
+function chunk(array,size){
+  let newArr = [];
+  while(array.length){
+    newArr.push(array.splice(0,size))
+  }
+  return newArr;
+}
+
 
 module.exports = chunk;
