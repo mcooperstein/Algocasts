9cfe2d7adb2e6077f1f730c1dcc7fe730ae3a8be
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-13 16:08:28 -0800

    added another solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -25,5 +25,18 @@ function capitalize(str){
   }
   return words.join(' ');
 }
+// another solution
+
+function capitalize(str){
+  let result = str[0].toUpperCase()
+  for(let i =1; i <str.length; i++){
+    if(str[i-1]===" "){
+      result+=str[i].toUpperCase();
+    } else {
+      result+=str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
