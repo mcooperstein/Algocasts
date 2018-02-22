d1a07753ffd96f16d81c23ee3bc5dfa3cc14a8f9
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-22 12:32:59 -0800

    fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let arr = [0,1];
+  if(n===0){
+    return 0;
+  } else if(n===1){
+    return 1;
+  } else {
+    for(let i=2; i<=n; i++){
+      arr[i]=arr[i-2]+arr[i-1];
+    }
+  }
+  return arr[n];
+}
 
 module.exports = fib;
