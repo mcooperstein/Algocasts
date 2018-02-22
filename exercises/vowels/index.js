da815b6fc7a16a70724385ed6a7369da3dbb71ae
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-21 17:19:20 -0800

    added instructor solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,15 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
-  let numVowels = 0;
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  for(let i =0; i<str.length; i++){
-    if(vowels.includes(str[i].toLowerCase())){
-      numVowels++
-    }
-  }
-  return numVowels;
+// My Solution
+// function vowels(str) {
+//   let numVowels = 0;
+//   let vowels = ['a', 'e', 'i', 'o', 'u'];
+//   for(let i =0; i<str.length; i++){
+//     if(vowels.includes(str[i].toLowerCase())){
+//       numVowels++
+//     }
+//   }
+//   return numVowels;
+// }
+
+// Instructor Iterative Solution
+// function vowels(str){
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o','u'];
+//   for(let char of str.toLowerCase()){
+//     if(checker.includes(char)){
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// Instructor RegEx Solution
+
+function vowels(str){
+  const matches = str.match(/[aeiou]/gi);
+  //matches will either return an array of
+  //matches or null
+  return matches ? matches.length: 0;
 }
 
 module.exports = vowels;
