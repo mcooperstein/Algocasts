5e5d8377ca276a621ade190244d91891e977cf54
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-22 17:40:40 -0800

    qfroms using stacks and queues

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,34 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor(){
+    this.s1 = new Stack();
+    this.s2 = new Stack();
+  }
+  add(record){
+    this.s1.push(record);
+  }
+  remove(){
+    while(this.s1.peek()){
+      this.s2.push(this.s1.pop());
+    }
+    let removed =  this.s2.pop();
+    while(this.s2.length>0){
+      this.s1.push(this.s2.pop());
+    }
+    return removed;
+  }
+  peek(){
+    while(this.s1.peek()){
+      this.s2.push(this.s1.pop());
+    }
+    let peeked =  this.s2.peek();
+    while(this.s2.peek()){
+      this.s1.push(this.s2.pop());
+    }
+    return peeked;
+  }
+}
 
 module.exports = Queue;
